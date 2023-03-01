f64103cb53350103aaa4e62f70423950ee51483b
Paul Anthony McGowan <xhostcom@aol.com>
2023-03-01 14:00:28 +0000

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ series = Series.create(title: "Awesome series", duration: 10)
 
 10.times do |n|
     n = n + 1
-    Episode.create(title: "Episode #{n}", duration: 120, series: (n.odd? ? series : nil))
+    Episode.create(title: "Episode #{n}", duration: rand(500..1000), series: (n.odd? ? series : nil))
 end
 
 puts "All done"
